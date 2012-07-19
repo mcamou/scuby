e56ed0269c5ecbfa55c66dfbae41288f40a78e53
Mario Camou <mcamou@abstra.cc>
2012-07-19 20:39:56 +0200

    Added unit tests and updated the README with some new stuff that had been added to the code.

---
+++ src/test/resources/test.rb
@@ -0,0 +1,63 @@
+require 'java'
+
+class Person
+  attr_accessor :firstname, :lastname
+  def initialize (firstname, lastname)
+    @firstname = firstname
+    @lastname = lastname
+ end
+
+  def fullname
+    "#{firstname} #{lastname}"
+  end
+
+  def get_label
+    javax.swing.JLabel.new(fullname)
+  end
+
+  def ==(other)
+    other.firstname == firstname and other.lastname == lastname
+  end
+
+  def to_s
+    "Person(#{lastname}, #{firstname})"
+  end
+
+  def hash
+    firstname.hash ^ lastname.hash
+  end
+end
+
+module Backend
+ def self.get_people
+   [
+     Person.new("Zaphod","Beeblebrox"),
+     Person.new("Arthur","Dent"),
+     Person.new("Ford","Prefect")
+   ]
+  end
+
+  def self.get_data
+    { :people => get_people, :other_data => get_other_data }
+  end
+
+  def self.get_person(firstname)
+    p = get_people.select { | p | p.firstname == firstname}
+    if p.length > 0
+      p[0]
+    else
+      nil
+    end
+  end
+
+  def self.get_other_data
+    {
+      :system_name => "Deep Thought",
+      :answer => 42
+    }
+  end
+end
+
+def get_empty_array
+[]
+end
